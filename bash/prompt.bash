1704f7603610fc84d9be194b5715b597ad71955b
Michael Ferguson <mpherg@hotmail.com>
2013-10-29 20:30:20 -0600

    Remove name from prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -15,10 +15,10 @@ branch_name=$(tput bold; tput setaf 4)
 if [ -s "$HOME/.dotfiles/git/git-prompt.sh" ]
 then
 	source "$HOME/.dotfiles/git/git-prompt.sh"
- 	GIT_PS1_SHOWDIRTYSTATE=1
- 	GIT_PS1_SHOWSTASHSTATE=1
- 	GIT_PS1_SHOWUNTRACKEDFILES=1
- 	GIT_PS1_SHOWUPSTREAM=verbose
- 	GIT_PS1_SHOWCOLORHINTS=1
-	PROMPT_COMMAND='__git_ps1 "\[$black\]\u@\h:\[$reset\]\[$yellow$bold\]\w\[$reset\]" "\n[\!] \$ " " [%s]"'
+	GIT_PS1_SHOWDIRTYSTATE=1
+	GIT_PS1_SHOWSTASHSTATE=1
+	GIT_PS1_SHOWUNTRACKEDFILES=1
+	GIT_PS1_SHOWUPSTREAM=verbose
+	GIT_PS1_SHOWCOLORHINTS=1
+	PROMPT_COMMAND='__git_ps1 "\[$yellow\]\w\[$reset\]" "\n\$ " " [%s]"'
 fi
